6f23aedb769f8cbf7378c3ead94134f7f29f9d44
MarkedOlive <olivdev4@gmail.com>
2022-09-23 18:57:15 -0400

    Bug

--- api/index.js
+++ api/index.js
@@ -19,9 +19,8 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
 conn.sync({ force: true  }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log('%s listening at 3001');
   });
 });
