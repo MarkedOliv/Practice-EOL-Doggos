f758fa89c6d28ea8e1c25be1e33b8ff814d0b4b0
MarkedOlive <olivdev4@gmail.com>
2022-09-23 18:49:00 -0400

    Bug

--- api/src/db.js
+++ api/src/db.js
@@ -2,12 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
-} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
 
-let sequelize = 
-process.env.NODE_ENV === "production" ? new Sequelize({
+let sequelize = process.env.NODE_ENV === "production" ? new Sequelize({
   database: DB_NAME,
   dialect: "postgres",
   host: DB_HOST,
