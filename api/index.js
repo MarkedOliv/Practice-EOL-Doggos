6904247dec10a437da48f4c04bf857fbf37479da
MarkedOlive <olivdev4@gmail.com>
2022-09-23 19:27:10 -0400

    Changing port

--- api/index.js
+++ api/index.js
@@ -19,8 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 3001;
 conn.sync({ force: true  }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001');
   });
 });
