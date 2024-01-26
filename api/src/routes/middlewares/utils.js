60a479437ebddadf6589fa6e0c89881d1a608134
MarkedOliv <olivdev4@gmail.com>
2024-01-26 13:21:53 -0400

    Clean a little and trying to fix the bug

--- api/src/routes/middlewares/utils.js
+++ api/src/routes/middlewares/utils.js
@@ -1,71 +1,76 @@
 const axios = require("axios");
+
 const { Dogs, Temperaments } = require("../../db.js");
 const { API_KEY } = process.env;
+
 let apiDogs = `https://api.thedogapi.com/v1/breeds?api_key=${API_KEY}`;
 
-const getApiData = async() => {
-    
+const processDogData = (dog) => {
+
+  const temperamentArray = dog.temperament ? dog.temperament.split(", ") : [];
+  const heightArray = dog.height.metric ? dog.height.metric.split(" - ") : [];
+  const weightArray = dog.weight.metric ? dog.weight.metric.split(" - ") : [];
+
+  return {
+    id: dog.id,
+    name: dog.name,
+    height: heightArray,
+    weight: weightArray,
+    temperaments: temperamentArray,
+    life_span: dog.life_span,
+    image: dog.image.url,
+  };
+};
+
+const getApiData = async () => {
+  try {
     const apiData = await axios.get(apiDogs);
-    const apiInfo = await apiData.data.map(dog => {
-    let temperamentArray = [];
-    if (dog.temperament) {
-        temperamentArray = dog.temperament.split(", ");
-    }
+    const dogData = [];
     
-    let heightArray = [];
-    if (dog.height.metric) {
-        heightArray = dog.height.metric.split(" - ");
+    for(const dog of apiData.data) {
+      const processedDog = processDogData(dog);
+      dogData.push(processedDog);
     }
 
-    let weightArray = [];
-    if (dog.weight.metric) {
-        weightArray = dog.weight.metric.split(" - ");
-    }
-        return {
-            id: dog.id,
-            name: dog.name,
-            height: heightArray,
-            weight: weightArray,
-            temperaments: temperamentArray,
-            life_span: dog.life_span,
-            image: dog.image.url,
-        }
-    })
-return apiInfo;
-}
+    return dogData;
+  } catch (error) {
+    console.error("Error obtaining API data:", error.message)
+    throw error;
+  }
+};
 
 const getFromDb = async () => {
-    return await Dogs.findAll({
-        include: {
-            model: Temperaments,
-            attributes: ['name'],
-            through: {
-            attributes: [],
-            },
-        }
-    })
+  return await Dogs.findAll({
+    include: {
+      model: Temperaments,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
 };
 
 const getAllDogs = async () => {
-    const dataFromApi = await getApiData();
-    const dataFromDb = await getFromDb();
-    const allDataMixed = [...dataFromApi, ...dataFromDb];
-    return allDataMixed;
+  const dataFromApi = await getApiData();
+  const dataFromDb = await getFromDb();
+  const allDataMixed = [...dataFromApi, ...dataFromDb];
+  return allDataMixed;
 };
 
 const getAllTemperaments = async () => {
-    const temperamentsApi = await axios.get(apiDogs);
-    const temperaments = temperamentsApi.data.map(t => t.temperament);
-    const temps = temperaments.toString().split(",");
-    temps.forEach(t => {
-        let i = t.trim()
-        Temperaments.findOrCreate({where: { name: i }});
-    });
-    const allTemp = await Temperaments.findAll();
-    return allTemp;  
+  const temperamentsApi = await axios.get(apiDogs);
+  const temperaments = temperamentsApi.data.map((t) => t.temperament);
+  const temps = temperaments.toString().split(",");
+  temps.forEach((t) => {
+    let i = t.trim();
+    Temperaments.findOrCreate({ where: { name: i } });
+  });
+  const allTemp = await Temperaments.findAll();
+  return allTemp;
 };
 
 module.exports = {
-    getAllDogs,
-    getAllTemperaments,
-}
+  getAllDogs,
+  getAllTemperaments,
+};
